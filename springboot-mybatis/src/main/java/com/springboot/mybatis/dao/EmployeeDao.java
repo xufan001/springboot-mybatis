92b5dc72847bc51d3d2926e6e6a86e44dfc0626b
xufan001 <47624524+xufan001@users.noreply.github.com>
2019-07-03 22:56:01 +0800

    Update EmployeeDao.java

--- springboot-mybatis/src/main/java/com/springboot/mybatis/dao/EmployeeDao.java
+++ springboot-mybatis/src/main/java/com/springboot/mybatis/dao/EmployeeDao.java
@@ -10,6 +10,7 @@ import java.util.List;
 @Mapper
 public interface EmployeeDao {
 
-    @Select("select * from employee")
+   // @Select("select * from employee")
+    //简单sql可使用如上注解
     List<Employee> findAll();
 }
