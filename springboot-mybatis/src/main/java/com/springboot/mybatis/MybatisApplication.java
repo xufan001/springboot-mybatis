4a5f838c3873790dbb612972a48c9e8714fb8190
xufan001 <47624524+xufan001@users.noreply.github.com>
2019-07-03 22:57:00 +0800

    Update MybatisApplication.java

--- springboot-mybatis/src/main/java/com/springboot/mybatis/MybatisApplication.java
+++ springboot-mybatis/src/main/java/com/springboot/mybatis/MybatisApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan(basePackages = "com.crud.dao")
 public class MybatisApplication {
 
     public static void main(String[] args) {
